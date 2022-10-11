0cab4f9ce090a7eef3e42170cc753f2c801ff47e
sathiyaseelan <sathiyaseelan2598@gmail.com>
2022-10-11 13:55:17 +0530

    assignment 2

---
+++ Customer.java
@@ -0,0 +1,10 @@
+class Customer{
+
+public static void main(String arg[]){
+
+    StarCinemas cinema=new StarCinemas();
+    cinema.bookTicket(200);
+
+}
+
+}
---
+++ StarCinemas.java
@@ -0,0 +1,7 @@
+class StarCinemas{
+
+void bookTicket(int amount){
+    System.out.println("balance amount "+(amount-120));
+}
+
+}
