0c19583c10c703850712872c75a00d381b44bc05
sathiyaseelan <sathiyaseelan2598@gmail.com>
2022-12-03 10:26:18 +0530

    date calculation

--- LoopStatement/src/com/myproject/date/DateCalculation.java
+++ LoopStatement/src/com/myproject/date/DateCalculation.java
@@ -9,6 +9,10 @@ public class DateCalculation {
 
 	public static void main(String[] args) throws ParseException {
 		// TODO Auto-generated method stub
+		int secs=60;
+		int mins=60;
+		int hrs=24;
+		int yrs=365;
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter your date of birth (dd/mm/yyyy)");
 		String birth=sc.next();
@@ -18,8 +22,8 @@ public class DateCalculation {
 		Date d1=sd.parse(birth);
 		Date d2=sd.parse(current);
 		long time=d2.getTime()-d1.getTime();
-		long year=(time/(1000l*60*60*24*365));
-		long day=(time/(1000*60*60*24))%365;
+		long year=(time/(1000l*secs*mins*hrs*yrs));
+		long day=(time/(1000*secs*mins*hrs))%yrs;
 		System.out.println(year +" years "+ day+" days");
 	}
 
