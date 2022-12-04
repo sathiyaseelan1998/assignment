7be4d555ce8a4d021e637cd8e563acbc04f0c1a6
sathiyaseelan <sathiyaseelan2598@gmail.com>
2022-12-04 09:36:47 +0530

    multiple catch

--- LoopStatement/src/com/exception/ExceptionHandling.java
+++ LoopStatement/src/com/exception/ExceptionHandling.java
@@ -1,11 +1,63 @@
 package com.exception;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class ExceptionHandling {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ExceptionHandling eh=new ExceptionHandling();
-		eh.tryCatch();
+		//eh.tryCatch();
+		//eh.multipleCatch();
+		eh.multipleTry();
+	}
+
+	private void multipleTry() {
+		// TODO Auto-generated method stub
+		try {
+			System.out.println("hi"+10/5);
+			try {
+				String s="virat";
+				System.out.println(s.charAt(7));
+			}
+			catch (StringIndexOutOfBoundsException e) {
+				System.out.println("name index small");
+			}
+		}
+		
+		catch (ArithmeticException e) {
+			System.out.println("0 not use by divisor");
+		}
+	}
+
+	private void multipleCatch() {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter no");
+		int no1=0;
+		int no2=0;
+		int[] ar=new int[3];
+		try {
+			int[] a=new int[-3];
+			no1=sc.nextInt();
+			no2=sc.nextInt();
+			System.out.println(no1/no2);
+			ar[3]=10;
+		}
+		catch(ArithmeticException e) {
+			System.out.println("chek no2");
+		}
+		catch(InputMismatchException e) {
+			System.out.println("enter number only");
+		}
+		catch(NegativeArraySizeException e) {
+			System.out.println("should be not index negative");
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
+		
 	}
 
 	private void tryCatch() {
