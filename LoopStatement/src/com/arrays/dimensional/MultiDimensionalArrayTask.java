f49a47575c859c4ca07c07f590547cde1516106f
sathiyaseelan <sathiyaseelan2598@gmail.com>
2022-12-07 08:16:36 +0530

    array

--- LoopStatement/src/com/arrays/dimensional/MultiDimensionalArrayTask.java
+++ LoopStatement/src/com/arrays/dimensional/MultiDimensionalArrayTask.java
@@ -17,45 +17,74 @@ public class MultiDimensionalArrayTask {
 
 	private void spiral() {
 		// TODO Auto-generated method stub
-		Scanner sc=new Scanner(System.in);
-		System.out.println("enter no of row and column");
-		int no=sc.nextInt();
-		int spiral[][]=new int[no][no];
-		int value=1;
-		int minCol=0;
-		int maxCol=no-1;
-		int minRow=0;
-		int maxRow=no-1;
-		while(no*no>=value) {
-			for(int i=minCol;i<=maxCol;i++) {//left to right
-				spiral[minRow][i]=value;
-				value++;
-			}
-			for(int i=minRow+1;i<=maxRow;i++) {//up to down
-				spiral[i][maxCol]=value;
-				value++;
-			}
-			for(int i=maxCol-1;i>=minCol;i--) {//right to left
-				spiral[maxRow][i]=value;
-				value++;
-			}
-			for(int i=maxRow-1;i>=minRow;i--) {//down  to up
-				spiral[i][minCol]=value;
-				value++;
-			}
-			minCol++;
-			minRow++;
-			maxCol--;
-			maxRow--;
-		}
-		for(int i=0;i<spiral.length;i++) {
-			for(int j=0;j<spiral[i].length;j++) {
-				System.out.print(spiral[i][j]+" ");
-			
-			}
-			System.out.println();
-		}
-	}
+		System.out.println("Enter The Value For N :");
+        
+        Scanner sc = new Scanner(System.in);
+         
+        int n = sc.nextInt();
+         
+        int[][] spiral = new int[n][n];
+         
+        int value = 1;
+         
+        int minCol = 0;
+         
+        int maxCol = n-1;
+         
+        int minRow = 0;
+         
+        int maxRow = n-1;
+         
+        while (value <= n*n)
+        {
+            for (int i = minCol; i <= maxCol; i++)
+            {
+                spiral[minRow][i] = value;
+                     
+                value++;
+            }
+             
+            for (int i = minRow+1; i <= maxRow; i++) 
+            { 
+                spiral[i][maxCol] = value; 
+                 
+                value++; 
+            } 
+             
+            for (int i = maxCol-1; i >= minCol; i--)
+            {
+                spiral[maxRow][i] = value;
+                             
+                value++;
+            }
+             
+            for (int i = maxRow-1; i >= minRow+1; i--) 
+            {
+                spiral[i][minCol] = value;
+                 
+                value++;
+            }
+             
+            minCol++;
+             
+            minRow++;
+             
+            maxCol--;
+             
+            maxRow--;
+        }
+         
+        for (int i = 0; i < spiral.length; i++)
+        {
+            for (int j = 0; j < spiral.length; j++)
+            {
+                System.out.print(spiral[i][j]+ "\t");
+            }
+             
+            System.out.println();
+        }
+    }
+	
 
 	private void highestMarkOfEachExam() {
 		// TODO Auto-generated method stub
