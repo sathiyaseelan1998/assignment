7f346e15fe91b5c6af78b58274ef67520023473d
sathiyaseelan <sathiyaseelan2598@gmail.com>
2023-02-23 07:44:27 +0530

    multi-dimensional

--- LoopStatement/src/com/test/Test.java
+++ LoopStatement/src/com/test/Test.java
@@ -15,7 +15,31 @@ public class Test {
 		//t.allDuplicate(name);
 		//t.removeElement();
 		//t.moveLeft();
-		t.moveRight();
+		//t.moveRight();
+		//t.multiDimensional();
+		t.jagged();
+	}
+
+	private void jagged() {
+		// TODO Auto-generated method stub
+		int[][] ar= {{1,2,4,5},{3,4,5},{1,2},{1}};
+		for(int i=0;i<ar.length;i++) {
+			for(int j=0;j<ar[i].length;j++) {
+				System.out.print(ar[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+
+	private void multiDimensional() {
+		// TODO Auto-generated method stub
+		int[][] ar= {{1,2,4,5},{3,4,5,6}};
+		for(int i=0;i<ar.length;i++) {
+			for(int j=0;j<ar[i].length;j++) {
+				System.out.print(ar[i][j]+" ");
+			}
+			System.out.println();
+		}
 	}
 
 	private void moveRight() {
